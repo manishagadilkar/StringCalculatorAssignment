168e9d513a909f392d9be83c8fa9fc59c181303e
Manisha Gadilkar <manishagadikar93@gmail.com>
2024-03-14 23:05:59 +0530

    Added Additional Test for Invalid Inputs and Other Scenarios

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -41,5 +41,39 @@ RSpec.describe StringCalculator do
         expect { calculator.add('1,-2,-3') }.to raise_error('negative numbers not allowed: -2, -3')
       end
     end
+
+    context 'when input ends with a comma followed by a newline' do
+      it 'raises an "input is invalid" exception' do
+        expect { calculator.add("1,\n") }.to raise_error('input is invalid')
+      end
+    end
+
+    context 'when negative numbers are used' do
+      it 'raises an exception' do
+        calculator = StringCalculator.new
+        expect { calculator.add('1, -2') }.to raise_error(RuntimeError, 'negative numbers not allowed: -2')
+      end
+    end
+
+    context 'when the string contains a different delimiter' do
+      it 'handles a custom single-character delimiter' do
+        calculator = StringCalculator.new
+        expect(calculator.add("//;\n1;2")).to eq(3)
+      end
+    end
+
+    context 'when the string has new lines between numbers' do
+      it 'returns the sum' do
+        calculator = StringCalculator.new
+        expect(calculator.add("1\n2,3")).to eq(6)
+      end
+    end
+
+    context 'when the string contains numbers' do
+      it 'returns the sum of the numbers' do
+        calculator = StringCalculator.new
+        expect(calculator.add('1,2,3')).to eq(6)
+      end
+    end
   end
 end
