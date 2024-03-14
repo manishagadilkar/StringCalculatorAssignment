00e188703400a970aa112b8b3c3947dd89069dc8
Manisha Gadilkar <manishagadikar93@gmail.com>
2024-03-14 22:18:47 +0530

    Handled Invalid Input

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,9 @@
 class StringCalculator
   def add(numbers)
-     # Return 0 for empty strings
-     return 0 if numbers.empty?
+    # Return 0 for empty strings
+    return 0 if numbers.empty?
+     
+    # Check for invalid input where a number ends with a comma followed by a newline
+    raise 'input is invalid' if numbers.match?(/,\n|\n,/)
   end
 end
