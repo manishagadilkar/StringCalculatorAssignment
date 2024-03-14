a976a491f5db350203d2ad3472767d9d308f9ebd
Manisha Gadilkar <manishagadikar93@gmail.com>
2024-03-14 22:28:33 +0530

    Parse Custom Delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,5 +5,12 @@ class StringCalculator
      
     # Check for invalid input where a number ends with a comma followed by a newline
     raise 'input is invalid' if numbers.match?(/,\n|\n,/)
+
+    # Check and set custom delimiter if present, otherwise use default delimiters comma and newline
+    if numbers.start_with?('//')
+      delimiter, numbers = parse_custom_delimiter(numbers)
+    else
+      delimiter = /[,|\n]/
+    end
   end
 end
